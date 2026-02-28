1915d09acd59bb902a9d64acc40382047c08e272
Julia Hurtig <juliabeatrizsehnhurtig@gmail.comgit config --listgit config --global user.email>
2026-02-27 21:27:21 -0300

    adiciona backend express e página HTML da aula 27-02

--- aulas/20-02/index.js
+++ aulas/20-02/index.js
@@ -1,7 +1,15 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") return res.sendStatus(204);
+  next();
+});
 
 // "banco" em memória
 const usuarios = [];
